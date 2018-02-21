fe14dae208b27c334deaeb2701799f6d2a10adbd
Nayana-ibm <nthorat2us.ibm.com>
2018-02-21 09:52:09 +0000

    https://github.com/elastic/gosigar/issues/73 socket test fix for Big endian

---
+++ endian/endian.go
@@ -0,0 +1,14 @@
+package endian
+
+import "encoding/binary"
+import "unsafe"
+
+func GetEndian() binary.ByteOrder {
+	var i int32 = 0x1
+	v := (*[4]byte)(unsafe.Pointer(&i))
+	if v[0] == 0 {
+		return binary.BigEndian
+	} else {
+		return binary.LittleEndian
+	}
+}
