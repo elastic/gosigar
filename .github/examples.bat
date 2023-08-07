9cb5d98b656683294e03b579bcef8e1737468d64
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-08-07 11:26:11 +0200

    windows: iterate in batch through the list of examples

--- .github/examples.bat
+++ .github/examples.bat
@@ -1,7 +1,9 @@
-
-for %%exampleDir in (examples/df examples/free examples/ps examples/uptime) do (
-	go build -o %%exampleDir%%/out.exe ./%%exampleDir%%
-	cd %%exampleDir%%
+@echo off
+set examples=examples/df examples/free examples/ps examples/uptime
+(for %%i in (%examples%) do (
+    echo %%a
+	go build -o %%i/out.exe ./%%i
+	cd %%i
 	out.exe
 	cd ..
-)
+))
