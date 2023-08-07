4fc603a3cf2c9aae342d77340e6c504b485d2705
Victor Martinez <victormartinezrubio@gmail.com>
2023-08-07 11:35:08 +0200

    Apply suggestions from code review

--- .github/examples.bat
+++ .github/examples.bat
@@ -1,9 +1,5 @@
-@echo off
 set examples=examples/df examples/free examples/ps examples/uptime
 (for %%i in (%examples%) do (
-    echo %%a
 	go build -o %%i/out.exe ./%%i
-	pushd %%i
-	out.exe
-	popd
+	%%i/out.exe
 ))
