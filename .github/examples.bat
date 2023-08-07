e0f34709fddf3c0db86bc03ed4b1e4644056e43e
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-08-07 11:30:44 +0200

    use pushd popd

--- .github/examples.bat
+++ .github/examples.bat
@@ -3,7 +3,7 @@ set examples=examples/df examples/free examples/ps examples/uptime
 (for %%i in (%examples%) do (
     echo %%a
 	go build -o %%i/out.exe ./%%i
-	cd %%i
+	pushd %%i
 	out.exe
-	cd ..
+	popd
 ))
